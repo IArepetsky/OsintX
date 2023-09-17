e33e4c0a6e8bc9c92f81237d5882b4e8c959a41a
IArepetsky <132691071+IArepetsky@users.noreply.github.com>
2023-09-17 13:07:04 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,61 @@
+import time
+import os
+import subprocess
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear_console()
+
+print("Загрузка")
+
+time.sleep(1)
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear_console()
+
+
+print("Загрузка.")
+
+time.sleep(1)
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear_console()
+
+print("Загрузка..")
+
+time.sleep(1)
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear_console()
+
+print("Загрузка...")
+
+time.sleep(1)
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear_console()
+
+print("Добро пожаловать!")
+
+time.sleep(1)
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear_console()
+subprocess.run(["python", "main.py"])
