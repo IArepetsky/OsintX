b54d09351a61e8cb388e58592fe79918dcccb367
IArepetsky <132691071+IArepetsky@users.noreply.github.com>
2023-10-05 13:42:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,27 +8,53 @@ from phonenumbers import geocoder, carrier, timezone
 
 # Данные о номере телефона 2
 
-print(Fore.GREEN + " ")
-time.sleep(0.5)
+print(Fore.BLUE + " ")
+time.sleep(0.3)
 print("░█████╗░░██████╗██╗███╗░░██╗████████╗██╗░░██╗")
-time.sleep(0.5)
+time.sleep(0.3)
 print("██╔══██╗██╔════╝██║████╗░██║╚══██╔══╝╚██╗██╔╝")
-time.sleep(0.5)
+time.sleep(0.3)
 print("██║░░██║╚█████╗░██║██╔██╗██║░░░██║░░░░╚███╔╝░")
-time.sleep(0.5)
+time.sleep(0.3)
 print("██║░░██║░╚═══██╗██║██║╚████║░░░██║░░░░██╔██╗░")
-time.sleep(0.5)
+time.sleep(0.3)
 print("╚█████╔╝██████╔╝██║██║░╚███║░░░██║░░░██╔╝╚██╗")
-time.sleep(0.5)
+time.sleep(0.3)
 print("░╚════╝░╚═════╝░╚═╝╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝")
-time.sleep(1.5)
-print(Fore.YELLOW + "-----------------------------------------------------")
-print(Fore.BLUE + "OSINTX || Создатель - IArepetsky || Поддержка - Kero")
 
-print("1. Пробив по номеру                                      2. Наш дс")
-print("3. Пробив по IP                                          4. ...")
+time.sleep(1.0)
 
-a = input("Выберите действие: ")
+txt = Fore.YELLOW + '-----------------------------------------------------'
+for i in txt:
+    time.sleep(0.1)
+    print(i, end='', flush=True)
+
+print(" ")
+
+txt1 = Fore.GREEN + 'OSINTX || Создатель - IArepetsky || Поддержка - Kero'
+for i in txt1:
+    time.sleep(0.1)
+    print(i, end='', flush=True)
+
+print("")
+
+txt2 = Fore.GREEN + '[1] Пробив по номеру   [2] Пробив по IP   [3] Наш Дс'
+for i in txt2:
+    time.sleep(0.03)
+    print(i, end='', flush=True)
+
+print("")
+print(" ")
+
+txt3 = Fore.GREEN + '[*] Выберите Действие:'
+for i in txt3:
+    time.sleep(0.05)
+    print(i, end='', flush=True)
+   
+a = input(" ") 
+
+
+
 if a == "1":
    def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -37,7 +63,7 @@ if a == "1":
    clear_console()
    subprocess.run(["python", "phone.py"])
 
-if a == "3":
+if a == "2":
    def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -45,7 +71,7 @@ if a == "3":
    clear_console()
    subprocess.run(["python", "ip.py"])
 
-if a == "2":
+if a == "3":
    def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
